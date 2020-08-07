b8c117b7769183e9c212119de68673c823754559
andy <844729361@qq.com>
2020-08-07 21:32:00 +0800

    5.4 小程序注册用户

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,5 @@
 import wepy from '@wepy/core'
-import { login, refresh, logout } from '@/api/auth'
+import { login, refresh, logout, register } from '@/api/auth'
 import { getCurrentUser } from '@/api/user'
 import * as auth from '@/utils/auth'
 import isEmpty from 'lodash/isEmpty'
@@ -58,6 +58,15 @@ const actions = {
     // 清空 storage
     auth.logout()
     commit('resetState')
+  },
+  
+  async register ({ dispatch }, params = {}) {
+    const loginData = await wepy.wx.login()
+    params.code = loginData.code
+
+    await register(params)
+
+    await dispatch('login')
   }
 }
 
